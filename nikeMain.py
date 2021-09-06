ac40f59d38ba4138faeaa0e9d1a47a3ba1ad87a2
mandoo1027 <40375307+mandoo1027@users.noreply.github.com>
2021-09-06 21:51:47 +0900

    Add files via upload

--- nikeMain.py
+++ nikeMain.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import bs4.element
-import datetime
+from datetime import datetime
 import time
 import numpy
+import schedule
+
 
 import firebase_admin
 from firebase_admin import credentials
@@ -12,7 +14,7 @@ cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://nikearlam-default-rtdb.firebaseio.com/'
 })
-
+fireKeyList = []
 # BeautifulSoup 객체 생성
 def get_soup_obj(url):
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; ; NCLIENT50_AAPDC42CAF9285) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36'}
@@ -20,7 +22,7 @@ def get_soup_obj(url):
     soup = BeautifulSoup(res.text,'lxml')
     return soup
 
-def isExist(list , key):
+def isExist(list , key):#등록하려는 배열에 중복제거
     isOverlap = False
     for data in list:
         if(data.productKey == key):
@@ -28,6 +30,12 @@ def isExist(list , key):
             break
         
     return isOverlap
+
+def isFireExist(key):#파이어 베이스 DB안에 중복되는 데이터 있을 경우 제외
+    if(fireKeyList.index(key) > -1):
+        return True
+    else:
+        return False
 
 def getProductInfo(productList): 
     nikeList = []
@@ -44,28 +52,29 @@ def getProductInfo(productList):
         if None != a.find('img') : imageSrc = a.find('img').attrs.get('data-src')
 
         if(monthStr != '') :
-            if(isExist(nikeData,productKey)):
-                nikeData = {
-                    "productKey" : productKey
-                    , "productUrl" : productUrl
-                    , "productName": productName
-                    , "monthStr"   : monthStr
-                    , "dayStr"     : dayStr
-                    , "imageSrc"   : imageSrc
-                }
-                nikeList.append(nikeData)
+            if(isExist(nikeList,productKey) == False):
+                if(isFireExist(productKey) == False):
+                    nikeData = {
+                        "productKey" : productKey
+                        , "productUrl" : productUrl
+                        , "productName": productName
+                        , "monthStr"   : monthStr
+                        , "dayStr"     : dayStr
+                        , "imageSrc"   : imageSrc
+                    }
+                    nikeList.append(nikeData)
     return nikeList
 
 
 def saveNike(nikeList):
     for nike in nikeList:
-        year = nike['year'] 
+        productKey = nike['productKey'] 
         month = nike['month']
         
         imageSrc = nike['imageSrc']
-        pathStr = '/nike/'+year+month
+        pathStr = '/nike/'+productKey
         ref = db.reference(pathStr)
-        ref.push(nike)
+        ref.set(nike)
 
 
 # 나이키 목록 가져오기
@@ -84,7 +93,7 @@ def getDetailInfo(productList):
         productName  = product.find('a', 'card-link').attrs.get('title')
         productUrl   = 'https://www.nike.com' + product.find('a', 'card-link').attrs.get('href')
         imageSrc     = product.find('img', 'img-component').attrs.get('data-src')
-        eventDate = product.attrs.get('data-active-date')
+        eventDate = product.attrs.get('data-active-date')+ ":00"
         dateStr = eventDate.split(' ')[0].replace('/','')
         timeStr = eventDate.split(' ')[1]
         yyyy = eventDate.split(' ')[0].split('/')[0]
@@ -110,13 +119,26 @@ def getDetailInfo(productList):
             nikeList.append(nikeData)
     return nikeList
 
-        
+def getFireNikeData():
+    ref = db.reference('nike')
+    snapshot = ref.get()
+    if(snapshot != None):
+        for key, val in snapshot.items():
+            fireKeyList.append(key)
 
-def callNikeData(monthList):
+def callNikeData():
     nikeData = getNikeData()
     saveNike(nikeData)
 
+def job():
+    print("호출")
+    getFireNikeData()
+    callNikeData()
+
 if __name__ == "__main__":
-    
-    monthList = [8,9,10]
-    callNikeData(monthList)
+    job()
+    #schedule.every().day.at("23:21").do(job)
+    #while True:
+    #    schedule.run_pending()
+    #    time.sleep(1)
+
